f951868b42d892e7723ccfbe0e5567634fff5751
Nikita Silivonchik <lstpsche@gmail.com>
2019-11-04 00:47:53 +0300

    Rewrite generating of link to schedule creation

--- app/actions/features/schedules/create_schedule.rb
+++ app/actions/features/schedules/create_schedule.rb
@@ -22,18 +22,37 @@ module Actions
         def create_button_with_url(text)
           Telegram::Bot::Types::InlineKeyboardButton.new(
             text: text,
-            url: ENV['WEB_CREATE_SCHEDULE_URL']
+            url: generate_link_to_schedule_creation
           )
         end
 
         def create_markup(markup_options)
+          link_button_text = I18n.t('actions.features.schedules.create_schedule.button_text')
           super(markup_options) do
-            create_button_with_url('Go to web version')
+            create_button_with_url(link_button_text)
           end
         end
 
         def message_text
           I18n.t('actions.features.schedules.create_schedule.header')
+        end
+
+        def user_auth_params
+          {
+            user_id: user.id,
+            user_token: user.authentication_token
+          }
+        end
+
+        def generate_link_to_schedule_creation
+          link = ENV['WEB_VERSION_URL'] + I18n.t('web_version_links.new_schedule') + '?'
+
+          user_auth_params.each do |key, value|
+            link += "#{key}=#{value}&"
+          end
+
+          link.slice!(-1)
+          link
         end
       end
     end
